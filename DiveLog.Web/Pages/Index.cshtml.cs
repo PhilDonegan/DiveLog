979b9668105acc7fcce9f4cfadceca754890c1c3
Phil Donegan <phil.dunny@hotmail.com>
2019-06-13 21:35:36 +0100

    Added simple file upload ready for dive log parsers

--- DiveLog.Web/Pages/Index.cshtml.cs
+++ DiveLog.Web/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DiveLog.Web.Helpers;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,27 @@ namespace DiveLog.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        [BindProperty]
+        public IFormFile DiveProfilesFile { get; set; }
 
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var diveProfiles = await FileHelpers.ProcessFormFile(DiveProfilesFile, ModelState);
+            Console.WriteLine(diveProfiles);
+            
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ViewData["DiveProfiles"] = diveProfiles;
+            return Page();
         }
     }
 }
