b1f53ada09c34d146dc1902b6d94ae6444dbb73f
Phil Donegan <phil.dunny@hotmail.com>
2019-06-26 23:04:41 +0100

    Added helper to consume api service, need to think about what Uri to use in different environments

--- DiveLog.Web/Helpers/APIHelper.cs
+++ DiveLog.Web/Helpers/APIHelper.cs
@@ -2,15 +2,31 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace DiveLog.Web.Helpers
 {
     public class APIHelper
     {
-        public static async Task<bool> UploadDivesToAPI(IEnumerable<LogEntryDTO> dives)
+        private static HttpClient _client = new HttpClient();
+
+        static APIHelper()
         {
-            return true;
+            _client.BaseAddress = new Uri("https://divelogapi20190611082926.azurewebsites.net");
+            _client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async static Task<bool> UploadDivesToAPI(IEnumerable<LogEntryDTO> dives)
+        {
+            var response = await _client.GetAsync("api/LogEntries");
+            if (response.IsSuccessStatusCode)
+            {
+                var entries = await response.Content.ReadAsStringAsync();
+                return true;
+            }
+
+            return false;
         }
     }
 }
