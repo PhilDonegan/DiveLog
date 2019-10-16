c9fec65d1191336456760a7929b8034b3f23b2a4
Phil Donegan <phil.dunny@hotmail.com>
2019-10-16 21:38:40 +0100

    Added basic scaffolding for comparison tool

---
+++ DiveLog.GUI/Controllers/LogComparisonController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DiveLog.GUI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DiveLog.GUI.Controllers
+{
+    public class LogComparisonController : Controller
+    {
+        public IActionResult LogComparison()
+        {
+            return View();
+        }
+
+		public IActionResult LogComparisonResults(LogComparisonModel model)
+		{
+			return RedirectToAction("LogComparison");
+		}
+	}
+}
