a90b75bad8dbf3b7c352dee77eff86a25d69ea85
Phil Donegan <phil.dunny@hotmail.com>
2019-10-21 21:38:05 +0100

    Hacky method for back loading dives, need to refactor but will do for now.

--- DiveLog.GUI/Controllers/LogComparisonController.cs
+++ DiveLog.GUI/Controllers/LogComparisonController.cs
@@ -29,6 +29,12 @@ namespace DiveLog.GUI.Controllers
 
 		public async Task<IActionResult> LogComparisonResults(LogComparisonModel model)
 		{
+			//return await OriginalComparison(model);
+			return await EndLoadedComparison(model);
+		}
+
+		private async Task<IActionResult> EndLoadedComparison(LogComparisonModel model)
+		{
 			var dives = await _apiHelper.CompareDives(model.Depth, model.Time, model.DiveType);
 
 			if (!ModelState.IsValid)
@@ -37,10 +43,52 @@ namespace DiveLog.GUI.Controllers
 			}
 
 			var resultsModel = new LogComparisonResultsModel();
-			foreach(var dive in dives)
+			if (dives.Any())
+			{
+				var longestDive = dives.OrderByDescending(x => x.DiveLength).First();
+				foreach (var dive in dives)
+				{
+					var diveProfile = new DiveProfile();
+					var diveOffset = longestDive.DiveLength.Subtract(dive.DiveLength);
+					var orderedDatapoints = dive.DataPoints.OrderBy(x => x.Time).ToList();
+					var logInterval = orderedDatapoints[1].Time - orderedDatapoints[0].Time;
+
+					var blankValuesCount = diveOffset.TotalSeconds / logInterval;
+					var count = 0;
+					while (blankValuesCount > 0)
+					{
+						diveProfile.Datapoints.Add(new DataPoint(count * logInterval, 0));
+						count++;
+						blankValuesCount--;
+					}
+
+					foreach (var dp in orderedDatapoints)
+					{
+						var datapoint = new DataPoint(dp.Time + diveOffset.TotalSeconds, Convert.ToDouble(dp.Depth));
+						diveProfile.Datapoints.Add(datapoint);
+					}
+
+					resultsModel.DiveProfiles.Add(diveProfile);
+				}
+			}
+
+			return View(resultsModel);
+		}
+
+		private async Task<IActionResult> OriginalComparison(LogComparisonModel model)
+		{
+			var dives = await _apiHelper.CompareDives(model.Depth, model.Time, model.DiveType);
+
+			if (!ModelState.IsValid)
+			{
+				return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+			}
+
+			var resultsModel = new LogComparisonResultsModel();
+			foreach (var dive in dives)
 			{
 				var diveProfile = new DiveProfile();
-				foreach(var dp in dive.DataPoints.OrderBy(x => x.Time))
+				foreach (var dp in dive.DataPoints.OrderBy(x => x.Time))
 				{
 					var datapoint = new DataPoint(dp.Time, Convert.ToDouble(dp.Depth));
 					diveProfile.Datapoints.Add(datapoint);
